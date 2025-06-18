a9d9cd4a16b2902b6386ab27ba31de255977146e
BoxToc <masahuduanwar@gmail.com>
2025-06-17 22:15:25 -0500

    Initial commit

---
+++ custom_functions/helpers/config.js
@@ -0,0 +1,9 @@
+const config = {
+    HDB_USER:"masahuduanwar",
+    HDB_PASSWORD:"2481632@Anwar",
+    EMAIL_USER:"linkonmaso@gmail.com",
+    EMAIL_PASSWORD:"sxcd fsev pwcx xybr",
+    EMAIL_RECIPIENT:"masahuduanwar@gmail.com"
+};
+
+module.exports = config;
